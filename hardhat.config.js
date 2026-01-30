9bbfaedc4e14029bdf3e6b8d12428a9f6c01acce
Sagor <161376518+0xsagor@users.noreply.github.com>
2026-01-30 14:06:19 +0600

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.g.alchemy.com/v2/YOUR_API_KEY",
+      }
+    },
+    mainnet: {
+      url: "https://eth-mainnet.g.alchemy.com/v2/YOUR_API_KEY",
+      accounts: ["0xYOUR_PRIVATE_KEY"]
+    }
+  }
+};
